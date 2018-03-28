2dac550895bdd0cbb60f7303b6eb746ae9dd8494
liannesr <lianne.sanchez@upr.edu>
2018-03-28 01:11:41 -0400

    Three routes from chats, "See my chats", "See a chat by group id" & "Add to chat"

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask
-
+from handlers.group import GroupHandler
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/MessageApp/login/menu/chats')
+def chats():
+    handler = GroupHandler()
+    return handler.getAllGroups()
+
+@app.route('/MessageApp/login/menu/chats/add')
+def addToGroup():
+    return 'Contact has been added to the group chat!'
+
+@app.route('/MessageApp/login/menu/chats/<int:id>')
+def getGroupByID(id):
+    return GroupHandler().getGroupById(id)
+
+#
 
 
 if __name__ == '__main__':
